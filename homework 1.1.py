ffb62d3015340a8b49164b5e48c3b536f50c1fc0
komkovs <skomkov9@mail.ru>
2021-09-18 08:33:30 +0300

    отступы

--- homework 1.1.py
+++ homework 1.1.py
@@ -1,8 +1,8 @@
-x  = int(input("Введите x"))
-y= int(input("Введите y"))
-if x>y:
+x = int(input("Введите x"))
+y = int(input("Введите y"))
+if x > y:
     print("1-ое число больше 2")
-if x<y:
+elif x < y:
     print("2-ое число больше 1")
-if x==y:
+elif x == y:
     print("числа равны")
