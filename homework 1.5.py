bd4234875ad1ca37bdcd8a541eb4def4bae8f199
komkovs <skomkov9@mail.ru>
2021-09-18 08:36:37 +0300

    отступы

--- homework 1.5.py
+++ homework 1.5.py
@@ -1,12 +1,12 @@
-x = int(input("Введите X"))
-y = int(input("Введите Y"))
-if x>0 and y>0:
+x = int(input("Введите x"))
+y = int(input("Введите y"))
+if x > 0 and y > 0:
     print("1 четверть")
-if x<0 and y>0:
+elif x < 0 and y > 0:
     print("2 четверть")
-if x<0 and y<0:
+elif x < 0 and y < 0:
     print("3 четверть")
-if x>0 and y<0:
+elif x > 0 and y < 0:
     print("4 четверть")
-if x==0 or y==0:
+elif x == 0 or y == 0:
     print("Точка лежит на оси")
