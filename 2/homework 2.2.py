6795f24761bf7047b3916c60c4421476b8e17379
komkovs <skomkov9@mail.ru>
2021-10-02 09:18:23 +0300

    итог

---
+++ 2/homework 2.2.py
@@ -0,0 +1,12 @@
+i = str(input())
+i = i.replace(" ","")
+s = list(i)
+n = list()
+for l in range(0,len(s)):
+    n.append(int(s[l]))
+n.sort()
+n.reverse()
+v = str()
+for l in range(0,len(n)):
+    v=v+str(n[l])
+print(v)
