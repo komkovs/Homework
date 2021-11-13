df7eb96d28bc95d28f35853ccfa868fa32cec878
komkovs <skomkov9@mail.ru>
2021-11-13 08:09:13 +0300

    1

---
+++ 3/homework 3.2.py
@@ -0,0 +1,12 @@
+def time(x):
+    if x == 12 or x == 1 or x == 2:
+        return("зима")
+    elif x > 2 and x < 6:
+        return("весна")
+    elif x > 5 and x <9 :
+        return("лето")
+    else:
+        return("осень")
+
+f = int(input())
+print(time(f))
