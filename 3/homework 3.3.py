6f0f1db03780b54f6712dc66e807a1bf8bb5b1f3
komkovs <skomkov9@mail.ru>
2021-11-13 08:27:31 +0300

    2

--- 3/homework 3.3.py
+++ 3/homework 3.3.py
@@ -1,9 +1,8 @@
 def function(x):
     t1, t2 = 0, 1
-    while x > 0:
+    for _ in range(x):
         print(t1, end = " ")
         t1, t2 = t2, t1 + t2
-        x = x - 1
 
 n = int(input())
 function(n)
