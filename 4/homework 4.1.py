82605dd1401035aea2486205ab91f47b3c1cccd3
komkovs <skomkov9@mail.ru>
2021-11-22 21:59:29 +0300

    2

--- 4/homework 4.1.py
+++ 4/homework 4.1.py
@@ -1,10 +1,14 @@
 def function():
-    f = input("Введите элементы списка\n")
-    b = list()
-    while f != "":
-        b.append(f)
-        f = input()
-    k = int(input("Введите k\n"))
-    b = b[-k:] + b[:-k]
-    return b
+    try:
+        f = input("Введите элементы списка\n")
+        b = list()
+        while f != "":
+            b.append(f)
+            f = input()
+        assert b != []
+        k = int(input("Введите k\n"))
+        b = b[-k:] + b[:-k]
+        return b
+    except AssertionError:
+        return "Долбан"
 print(function())
