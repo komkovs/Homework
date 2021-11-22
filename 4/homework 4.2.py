7a758f87fbafe1517652a09f554afc4a13c7c898
komkovs <skomkov9@mail.ru>
2021-11-22 22:11:30 +0300

    2

--- 4/homework 4.2.py
+++ 4/homework 4.2.py
@@ -1,11 +1,14 @@
 def function():
-    n = int(input("Введите n"))
-    if n == 0:
-        return 1
-    factorial = 1
-    while n > 1:
-        factorial *= n
-        n -= 1
-    return factorial 
- 
+    n = int(input("Введите n:\n"))
+    try:
+        assert n > -1
+        if n == 0:
+            return 1
+        factorial = 1
+        while n > 1:
+            factorial *= n
+            n -= 1
+        return factorial
+    except AssertionError:
+        return "Не вводи отрицательное" 
 print(function())
