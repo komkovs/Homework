2c0a666e83cceb834fea690984d5c3b0228dab62
komkovs <skomkov9@mail.ru>
2021-12-04 08:27:15 +0300

    Итог+тесты

--- 5/homework 5.py
+++ 5/homework 5.py
@@ -15,8 +15,19 @@ n = int(input("Введите искомый элемент:"))
 
 
 print(search(d, n))
-assert search([], 4) == None
-assert search([1, 2, 3, 4, 5, 6, 7], 1) == 0
-assert search([1, 2, 3, 4, 5, 6, 7], 0) == None
+ assert search([], 42) is None
+assert search([0], 0) == 0
+assert search([0], 1) is None
+assert search([-1, 0, 42], 0) == 1
+assert search([-42, 0, 42], 42) == 2
+assert search([-6, -5, -4, -3, -2, -1], -4) == 2
+assert search([1, 2, 3, 4, 5, 6], 4) == 3
 assert search([1, 2, 3, 4, 5, 6, 7], 4) == 3
-assert search([1, 2, 2, 4, 5, 6, 7], 2) == 1
+assert search([1, 2, 3, 4, 5], 7) is None
+assert search([1, 2, 3, 4, 5, 6], 7) is None
+assert search([42, 42, 42, 42, 42], 42) == 0
+assert search([-42, -42, -42, -42, -42], -42) == 0
+assert search([42, 42, 42, 42, 43], 43) == 4
+assert search([41, 42, 42, 42, 42], 41) == 0
+assert search([-2, -2, -1, 0, 1, 2, 2, 2], -1) == 2
+assert search([-2, -2, -1, 0, 1, 1, 2, 2], 1) == 4
